2b6c5483b9ac5d070d61501a1be867dff7b32ef4
tahminasp103 <tahminap-sp103@code.edu.az>
2025-06-17 19:34:38 -0700

    Delete routes directory
    Delete routes directory

--- routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createOrder } from '../controllers/orderController.js';
-import { userControlAuth } from '../middleware/authMiddleWare.js';
-
-const router = express.Router();
-
-router.post('/', userControlAuth, createOrder); // token yoxlanacaq və sifariş yaradılacaq
-
-export default router;
